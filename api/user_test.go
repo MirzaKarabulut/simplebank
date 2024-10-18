5fa826b1a06f76d1f5cb76ac5f5ed353b1b14064
MirzaKarabulut <mirzakara1234@gmail.com>
2024-10-18 16:03:18 +0300

    added strong unit test for create user and some fixing

--- api/user_test.go
+++ api/user_test.go
@@ -3,9 +3,11 @@ package api
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
 	mockdb "github.com/MirzaKarabulut/simplebank/db/mock"
@@ -16,6 +18,32 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type eqCreateUserParamsMatcher struct {
+	arg	db.CreateUserParams
+	password	string
+}
+
+func (e eqCreateUserParamsMatcher) Matches(x interface{}) bool {
+	arg, ok := x.(db.CreateUserParams)
+	if !ok {
+		return false
+	}
+	err := util.CheckPassword(e.password, arg.HashedPassword)
+	if err != nil {
+		return false
+	}
+	e.arg.HashedPassword = arg.HashedPassword
+	return reflect.DeepEqual(e.arg, arg)
+}
+
+func (e eqCreateUserParamsMatcher) String() string {
+	return fmt.Sprintf("Matches arg %v and password %v ", e.arg, e.password)
+}
+
+func EqCreateUserParams(arg db.CreateUserParams, password string) gomock.Matcher {
+	return eqCreateUserParamsMatcher{arg, password}
+}
+
 func TestCreateUserAPI(t *testing.T) {
 	user, password := randomUser(t)
 
@@ -24,27 +52,33 @@ func TestCreateUserAPI(t *testing.T) {
 		name	string
 		body	gin.H
 		buildStubs	func(store *mockdb.MockStore)
-		checkResponse	func(t *testing.T, recorder *httptest.ResponseRecorder)
+		checkResponse	func(recorder *httptest.ResponseRecorder)
 	}{
 		{
-			name: "Ok",
+			name: "OK",
 			body: gin.H{
-				"username": user.Username,
-				"password": password,
+				"username":  user.Username,
+				"password":  password,
 				"full_name": user.FullName,
-				"email": user.Email,
+				"email":     user.Email,
 			},
 			buildStubs: func(store *mockdb.MockStore) {
-				store.EXPECT().CreateUser(gomock.Any(), gomock.Any()).Times(1).Return(user, nil)
+				arg := db.CreateUserParams{
+					Username: user.Username,
+					FullName: user.FullName,
+					Email:    user.Email,
+				}
+				store.EXPECT().
+					CreateUser(gomock.Any(), EqCreateUserParams(arg, password)).
+					Times(1).
+					Return(user, nil)
 			},
-			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusOK, recorder.Code)
 				requireBodyMatchUser(t, recorder.Body, user)
 			},
-		},
-
-		// TODO: Other test case
-	}
+	},
+}
 
 	for i := range testCase {
 		tc := testCase[i]
@@ -67,7 +101,7 @@ func TestCreateUserAPI(t *testing.T) {
       require.NoError(t, err)
 
 			server.router.ServeHTTP(recorder, request)
-      tc.checkResponse(t, recorder)
+      tc.checkResponse(recorder)
 		})
 	}
 }
