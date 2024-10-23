784029c5f29097db6d1c735ab696d3801c010244
MirzaKarabulut <mirzakara1234@gmail.com>
2024-10-23 14:49:40 +0300

    added verifytoken func

--- token/jwt_maker.go
+++ token/jwt_maker.go
@@ -1,12 +1,12 @@
 package token
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 )
-
 
 const minSecretSizeKey = 32
 
@@ -24,7 +24,6 @@ func NewJWTMaker(secretKey string) (Maker, error) {
 	return &JWTMaker{secretKey}, nil
 }
 
-
 // CreateToken creates a new token for username and duration
 func(maker *JWTMaker)	CreateToken(username string, duration time.Duration) (string, error) {
 	payload, err := NewPayload(username, duration)
@@ -38,5 +37,25 @@ func(maker *JWTMaker)	CreateToken(username string, duration time.Duration) (stri
 
 // VerifyToken checks the given token is valid or not
 func(maker *JWTMaker)	VerifyToken(token string) (*Payload, error) {
-	// TODO
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+			if !ok {
+				return nil, ErrInvalidToken
+			}
+			return []byte(maker.secretKey), nil
+	}
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+	if err != nil {
+		vErr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(vErr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
+	}
+	
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+	return payload, nil
 }
