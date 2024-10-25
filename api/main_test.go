e76c1d997cbee5317a540ba3cc748ce08c1aa001
MirzaKarabulut <mirzakara1234@gmail.com>
2024-10-25 19:31:34 +0300

    rechanged by newTestServer

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,23 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/MirzaKarabulut/simplebank/db/sqlc"
+	"github.com/MirzaKarabulut/simplebank/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config {
+		SymmetricTokenKey: util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
