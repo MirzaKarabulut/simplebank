8dde8ae91522f916790b14fd27f9fce2ade51474
MirzaKarabulut <mirzakara1234@gmail.com>
2024-10-25 19:32:04 +0300

    added new login router and added router func for refactor

--- api/server.go
+++ api/server.go
@@ -1,19 +1,33 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/MirzaKarabulut/simplebank/db/sqlc"
+	"github.com/MirzaKarabulut/simplebank/token"
+	"github.com/MirzaKarabulut/simplebank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
-	store db.Store
-	router *gin.Engine
+	config			util.Config
+	store 			db.Store
+	tokenMaker	token.Maker
+	router 			*gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store : store}
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.SymmetricTokenKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker %d", tokenMaker)
+	}
+	server := &Server{
+		config: config,
+		store : store,
+		tokenMaker: tokenMaker,
+	}
 	router := gin.Default()
 	
 
@@ -22,6 +36,7 @@ func NewServer(store db.Store) *Server {
 	}
 
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -30,8 +45,24 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfers", server.createTransfer)
 	
 
+	server.setupRouter()
+	return server, nil
+}
+
+func(server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	router.POST("/transfers", server.createTransfer)
+
 	server.router = router
-	return server
 }
 
 func (server *Server) Start(address string) error {
