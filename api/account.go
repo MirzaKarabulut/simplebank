8c0dad374fde832ea7167953c16eaef68ba4f6b5
MirzaKarabulut <mirzakara1234@gmail.com>
2024-10-28 15:38:48 +0300

    added authorization for acc methods

--- api/account.go
+++ api/account.go
@@ -2,15 +2,17 @@ package api
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"net/http"
 
 	db "github.com/MirzaKarabulut/simplebank/db/sqlc"
+	"github.com/MirzaKarabulut/simplebank/token"
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,currency"`
 }
 
@@ -21,8 +23,9 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.CreateAccountParams {
-		Owner: req.Owner,
+		Owner: authPayload.Username,
 		Currency: req.Currency,
 		Balance: 0,
 	}
@@ -63,6 +66,14 @@ func (server *Server) getAccount(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesnt belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
 	ctx.JSON(http.StatusOK, account)
 }
 
@@ -73,12 +84,16 @@ type listAccountRequest struct {
 
 func (server *Server) listAccount(ctx *gin.Context) {
     var req listAccountRequest
+		fmt.Println("REQUEST",req)
     if err := ctx.ShouldBindQuery(&req); err != nil {
         ctx.JSON(http.StatusBadRequest, errorResponse(err))
         return
     }
 
+		authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+
     arg := db.ListAccountsParams{
+				Owner: authPayload.Username,
         Limit:  req.PageSize,
         Offset: (req.PageID - 1) * req.PageSize,
     }
@@ -104,12 +119,25 @@ func (server *Server) deleteAccount(ctx *gin.Context) {
 		return
 	}
 
-	err := server.store.DeleteAccount(ctx, req.ID)
+	account, err := server.store.GetAccount(ctx, req.ID)	
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
 			return
 		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesnt belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	err = server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
