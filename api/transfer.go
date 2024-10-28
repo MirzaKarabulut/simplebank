b44881b04ead440bfcc14cb14cdf7a0298ca33f6
MirzaKarabulut <mirzakara1234@gmail.com>
2024-10-28 15:41:10 +0300

    added auth for check the accounts by their auth payload

--- api/transfer.go
+++ api/transfer.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 
 	db "github.com/MirzaKarabulut/simplebank/db/sqlc"
+	"github.com/MirzaKarabulut/simplebank/token"
 	"github.com/gin-gonic/gin"
 )
 
@@ -29,11 +31,20 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		Amount: req.Amount,
 	}
 
-	if !server.validAccount(ctx, arg.FromAccountID, req.Currency) {
+	fromAccount, valid := server.validAccount(ctx, arg.FromAccountID, req.Currency)
+	if !valid {
 		return
 	}
 	
-	if !server.validAccount(ctx, arg.ToAccountID, req.Currency) {
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if fromAccount.Owner != authPayload.Username {
+		err := errors.New("from account doesnt belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	_, valid = server.validAccount(ctx, arg.FromAccountID, req.Currency)
+	if !valid {
 		return
 	}
 
@@ -46,22 +57,22 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 }
 
 // validation of currency of transfer accounts
-func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) bool {
+func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) (db.Account, bool) {
 	account, err := server.store.GetAccount(ctx, accountID)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return false
+			return account, false
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return false
+		return account, false
 	}
 
 	if account.Currency != currency {
 		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
     ctx.JSON(http.StatusBadRequest, errorResponse(err))
-    return false
+    return account, false
 	}
-	return true
+	return account, true
 }
